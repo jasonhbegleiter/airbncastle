b1d9e98aa494c32f9d7aac89dfea55924380449b
Bridget Burnett <sscott2203@gmail.com>
2018-06-04 20:07:12 +0300

    updated castle model for castle_photos

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -2,6 +2,7 @@ class Castle < ApplicationRecord
   belongs_to :city
   belongs_to :user
   has_many :castle_amenities
+  has_many :castle_photos
   has_many :amenities, through: :castle_amenities
   has_many :photos, through: :castle_photos
 end
