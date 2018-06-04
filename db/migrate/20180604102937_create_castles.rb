6c38e684b4088e8138a1e6e065c64d54d022cee9
Bridget Burnett <sscott2203@gmail.com>
2018-06-04 13:30:12 +0300

    added castle model

--- db/migrate/20180604102937_create_castles.rb
+++ db/migrate/20180604102937_create_castles.rb
@@ -8,8 +8,8 @@ class CreateCastles < ActiveRecord::Migration[5.2]
       t.integer :no_of_guests
       t.integer :no_of_bedrooms
       t.integer :no_of_bathrooms
-      t.references :user, foreign_key: true
       t.references :city, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
