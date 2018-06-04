4af748d3ab50b280941c62925c926305daeb5f61
Bridget Burnett <sscott2203@gmail.com>
2018-06-04 17:28:38 +0300

    added validations to models

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -3,4 +3,12 @@ class Castle < ApplicationRecord
   belongs_to :user
   has_many :castle_amenities
   has_many :amenities, through: :castle_amenities
+
+  validates :name, uniqueness: true, presence: true
+  validates :summary, presence: true
+  validates :price_per_night, presence: true
+  validates :address, presence: true
+  validates :no_of_guests, presence: true
+  validates :no_of_bedrooms, presence: true
+  validates :no_of_bathrooms, presence: true
 end
