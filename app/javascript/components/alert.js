4380084cccd19e2b5f97c421bafb4c5844a5bf6c
Bridget Burnett <sscott2203@gmail.com>
2018-06-06 18:16:11 +0300

    added avilability functionality

---
+++ app/javascript/components/alert.js
@@ -0,0 +1,11 @@
+import swal from 'sweetalert';
+
+function bindSweetAlertButtonDemo() {
+  const swalButton = document.getElementById('sweet-alert-demo');
+    swalButton.addEventListener('click', () => {
+
+      console.log('hello');
+    });
+}
+
+export { bindSweetAlertButtonDemo };
