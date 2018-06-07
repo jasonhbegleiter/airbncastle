9b64d1c8e39038615b5a23b8fb8c7d1f7b21cad2
Bridget Burnett <sscott2203@gmail.com>
2018-06-07 11:42:29 +0300

    Added availability alert on booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,18 +18,20 @@ class BookingsController < ApplicationController
     @booking.price = booking_params["no_of_nights"].to_i * @castle.price_per_night
     @booking.accepted = false
     @booking.start_date = Date.new(booking_params["start_date(1i)"].to_i, booking_params["start_date(2i)"].to_i, booking_params["start_date(3i)"].to_i)
-
-
     bookings = Booking.where(castle_id: params[:castle_id])
     new_booking_start = @booking.start_date.to_date
     new_booking_end = @booking.start_date.to_date + @booking.no_of_nights
-    @unavailable = available(bookings, new_booking_start, new_booking_end).include?(false)
-
+    unavailable = available(bookings, new_booking_start, new_booking_end).include?(false)
+    if unavailable
+      @unavailable = unavailable
+      render :new
+    else
       @booking.user = current_user
       @booking.castle = @castle
       @booking.completed = false
       @booking.save
       redirect_to booking_path(@booking)
+    end
   end
 
   private
